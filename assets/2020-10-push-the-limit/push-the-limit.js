fa29f7c1bbb19e05c226ffe9a9213dc4f69afb14
jefbinomed <jean.francois.garreau@gmail.com>
2020-11-03 14:22:48 +0100

    Site updated: 2020-11-03 14:22:46

---
+++ assets/2020-10-push-the-limit/push-the-limit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var PushTheLimitCusto = PushTheLimitCusto || function() {
+
+    JefHelper.loadCss('/assets/2020-10-push-the-limit/push-the-limit.css', 'push-the-limitcss');
+    
+    return {}
+
+}();
